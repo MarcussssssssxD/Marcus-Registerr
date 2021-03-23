cb804a033317463acdf4b3aef74adeccad8419e2
Glitch (hello-express) <none>
2021-03-23 14:20:54 +0000

    🏓☎️ Checkpoint ./komutlar/emojilikayıt.js:14888641/1254 ./idler.json:14888641/694

--- komutlar/emojilikayıt.js
+++ komutlar/emojilikayıt.js
@@ -1,12 +1,12 @@
 const Discord = require("discord.js"),
   client = new Discord.Client();
 const db = require("quick.db");
-const id = require("./idler.json");
+const id = require("../idler.json");
 
 module.exports.run = async (client, message, args) => {
   if (
     !message.member.hasPermission("ADMINISTRATOR") &
-    !message.member.roles.cache.get("")
+    !message.member.roles.cache.get(id.yetkili)
   )
     return message.channel
       .send(
@@ -22,7 +22,18 @@ module.exports.run = async (client, message, args) => {
     message.mentions.members.first() ||
     message.guild.members.cache.get(args[0]);
 
-  let uye = message.guild.member(üye);
+
+
+  if (!üye)
+    return message.channel
+      .send(
+        new Discord.MessageEmbed()
+          .setColor("RED")
+          .setDescription(`Lütfen Bir Kullanıcı Etiketleyiniz`)
+      )
+      .then(m => m.delete({ timeout: 3000 }));
+  
+    let uye = message.guild.member(üye);
   let logs = db.get(`kullanici.${uye.id}.islog`) || [];
   logs = logs.reverse();
   let ismleri =
@@ -36,15 +47,7 @@ module.exports.run = async (client, message, args) => {
           )
           .join("\n")
       : "Kaydı Bulunamadı!";
-
-  if (!üye)
-    return message.channel
-      .send(
-        new Discord.MessageEmbed()
-          .setColor("RED")
-          .setDescription(`Lütfen Bir Kullanıcı Etiketleyiniz`)
-      )
-      .then(m => m.delete({ timeout: 3000 }));
+  
   args = args.filter(a => a !== "" && a !== " ").splice(1);
   let isim = args
     .filter(arg => isNaN(arg))
@@ -101,7 +104,7 @@ module.exports.run = async (client, message, args) => {
               .setDescription(
                 `<a:basarili:796036099562012674> ${
                   üye.user
-                } adlı kullanıcının ismini başarılı bir şekilde \`₰ ${isim} | ${yas}\` yapıp, <@&${"823917110110715946"}>, <@&${"823917173483241472"}> rollerini verdim. Kullanıcının önceki isimleri\n${ismleri}`
+                } adlı kullanıcının ismini başarılı bir şekilde \`₰ ${isim} | ${yas}\` yapıp, <@&${id.erkekrol1}>, <@&${id.erkekrol2}> rollerini verdim. Kullanıcının önceki isimleri\n${ismleri}`
               )
           )
           .then(m => m.delete({ timeout: 15000 }));
@@ -115,7 +118,7 @@ module.exports.run = async (client, message, args) => {
               .setDescription(
                 `<a:basarili:796036099562012674> ${
                   üye.user
-                } adlı kullanıcının ismini başarılı bir şekilde \`₰ ${isim} | ${yas}\` yapıp, <@&${"823917173483241472"}>, <@&${"823917138178998333"}> rollerini verdim. Kullanıcının önceki isimleri\n${ismleri} `
+                } adlı kullanıcının ismini başarılı bir şekilde \`₰ ${isim} | ${yas}\` yapıp, <@&${id.kızrol1}>, <@&${id.kızrol2}> rollerini verdim. Kullanıcının önceki isimleri\n${ismleri} `
               )
           )
           .then(m => m.delete({ timeout: 15000 }));
@@ -132,12 +135,12 @@ module.exports.run = async (client, message, args) => {
     });
 
   const ekayıt = async () => {
-    üye.roles.remove("823917475317940244");
-    üye.roles.add("823917173483241472");
-    üye.roles.add("823917110110715946");
+    üye.roles.remove(id.kayıtsız);
+    üye.roles.add(id.erkekrol1);
+    üye.roles.add(id.erkekrol2);
     üye.setNickname(`₰ ${isim} | ${yas}`);
     client.channels.cache
-      .get("823910583714971761")
+      .get(id.genelsohbet)
       .send(
         `${üye.user} \`adlı üye sunucumuza kayıt oldu. Aramıza hoş geldin :)`
       )
@@ -146,17 +149,17 @@ module.exports.run = async (client, message, args) => {
     db.push(`kullanici.${üye.id}.islog`, {
       İsim: isim,
       Yaş: yas,
-      Rol: "823917110110715946"
+      Rol: id.erkekrol1
     });
   };
 
   const kkayıt = () => {
-    üye.roles.remove("823917475317940244");
-    üye.roles.add("823917173483241472");
-    üye.roles.add("823917138178998333");
+    üye.roles.remove(id.kayıtsız);
+    üye.roles.add(id.kızrol1);
+    üye.roles.add(id.kızrol2);
     üye.setNickname(`₰ ${isim} | ${yas}`);
     client.channels.cache
-      .get("823910583714971761")
+      .get(id.genelsohbet)
       .send(
         `${üye.user} \`adlı üye sunucumuza kayıt oldu. Aramıza hoş geldin :)`
       )
@@ -165,7 +168,7 @@ module.exports.run = async (client, message, args) => {
     db.push(`kullanici.${üye.id}.islog`, {
       İsim: isim,
       Yaş: yas,
-      Rol: "823917138178998333"
+      Rol: id.kızrol1
     });
   };
 };
